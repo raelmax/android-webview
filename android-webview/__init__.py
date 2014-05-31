41d56fdfce962eb72de2593a98568c1b776d655f
Rael Max <contato@raelmax.com>
2014-05-31 19:58:35 -0300

    added script to path

--- android-webview/__init__.py
+++ android-webview/__init__.py
@@ -1,12 +1,1 @@
-# -*- coding: utf-8 -*-
 
-# android create project --target 1 --name MyFirstApp --path MyFirstApp --activity MainActivity
-# --package com.example.myfirstapp
-
-app_name = raw_input("App Name: ")
-app_url = raw_input("App URL: ")
-package_name = raw_input("Package Name: ")
-target = raw_input("Target: ")
-
-print "#" * 100
-print "App Generated! Be happy!"
--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     author_email='contato@raelmax.com',
     url='https://github.com/raelmax/android-webview',
     packages=['android-webview'],
+    scripts=['android-webview/android-webview.py'],
     package_data={
         'android-webview': ['templates/*'],
     },
