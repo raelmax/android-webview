4a764ede39c68b53a2d35a4435b63fa49b89dbb0
Rael Max <contato@raelmax.com>
2014-05-31 21:20:43 -0300

    added manifest and mainactivity files

---
+++ android-webview/templates/MainActivity.java
@@ -0,0 +1,38 @@
+package {{package_name}};
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Window;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+ 
+public class MainActivity extends Activity {
+    private WebView mWebView;
+ 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        mWebView = new WebView(this);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl("{{app_url}}");
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+ 
+        this.setContentView(mWebView);
+    }
+ 
+    @Override
+    public boolean onKeyDown(final int keyCode, final KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+            mWebView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}r
