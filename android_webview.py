76d8c9b6010b3388d60eca667a9c5f436704db8e
Rael Max <contato@raelmax.com>
2014-06-02 09:16:51 -0300

    improve colors code

--- android_webview.py
+++ android_webview.py
@@ -5,18 +5,16 @@ import sys
 if os.system('which android') != 0:
     sys.exit('Android SDK is not installed. :(')
 
-COLORS = {
-    'blue': '\033[94m',
-    'green': '\033[92m',
-    'yellow': '\033[93m',
-    'red': '\033[91m',
-    'endc': '\033[0m'
+MSG= {
+    'success': '\033[92m{0}\033[0m',
+    'question': '\033[91m{0}\033[0m',
 }
 
-app_name = raw_input(COLORS['red'] + "App NAME: " + COLORS['endc'])
-app_url = raw_input(COLORS['red'] + "App URL: " + COLORS['endc'])
-app_package = raw_input(COLORS['red'] + "Package Name: " + COLORS['endc'])
+app_name = raw_input(MSG['question'].format('App NAME: '))
+app_url = raw_input(MSG['question'].format('App URL: '))
+app_package = raw_input(MSG['question'].format('Package Name: '))
 print '\n'
+
 package_dir = app_package.replace('.', '/')
 
 os.system('android create project --target 1 --name {0} --path {1} --activity MainActivity \
@@ -39,5 +37,4 @@ new_main_activity = open('{0}/src/{1}/MainActivity.java'.format(app_name, packag
 new_main_activity.write(main_activity_webview)
 new_main_activity.close()
 print '\n'
-print COLORS['green'] + 'App generated! Open project on Eclipse and run! :)' + COLORS['endc']
-print '\n'
+print MSG['success'].format('App generated! Open project on Eclipse and run! :)')
